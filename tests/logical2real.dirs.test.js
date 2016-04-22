c172c4d21534dc5979b103040d8b1cce0877f7f7
Mostafa Samir <mostafa.3210@gmail.com>
2016-04-22 18:32:14 +0200

    added tests for logical dirs handeling  in zip files

---
+++ tests/logical2real.dirs.test.js
@@ -0,0 +1,36 @@
+/*
+* Theses tests check the library coverage of logical directories in zip files
+* The library should always convert such directories to real ones to ensure
+* an error free experince when unzipping these files to disk
+*/
+
+var zipper = require('./../main')
+var expect = require('chai').expect;
+
+describe("Logical to Real Directories Conversion", function() {
+
+    it('converts logical directories in an existing zip file (async)', function(done) {
+        zipper.unzip('./tests/assets/folders.zip', function(error, unzipped) {
+            expect(error).to.be.null;
+            expect(unzipped.lowLevel().files).to.include.keys('folder1/');
+            expect(unzipped.lowLevel().files['folder1/'].dir).to.be.true;
+            done();
+        });
+    });
+
+    it('converts logical directories in an existing zip file (sync)', function() {
+        var unzipped = zipper.sync.unzip('./tests/assets/folders.zip')
+        expect(unzipped.lowLevel().files).to.include.keys('folder1');
+        expect(unzipped.lowLevel().files['folder1/'].dir).to.be.true;
+        done();
+    });
+
+    it('converts logical directories in low-level added files', function() {
+        var unzipped = zipper.sync.unzip('./tests/assets/hello.zip');
+        unzipped.lowLevel().file('logical/test.txt', 'logical');
+
+        expect(unzipped.lowLevel().files).to.include.keys('logical/');
+        expect(unzipped.lowLevel().files['logical/'].dir).to.be.true;
+    });
+
+});
